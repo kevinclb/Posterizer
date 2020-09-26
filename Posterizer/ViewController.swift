5d7fbeebe07b94b6afb963690d1e592b4e9b435c
Kevin Babou <kevincbabou@gmail.com>
2020-09-26 18:23:04 -0400

    Initial Commit

---
+++ Posterizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Posterizer
+//
+//  Created by Kevin Babou on 9/26/20.
+//  Copyright © 2020 Kevin Babou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
